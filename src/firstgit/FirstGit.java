bec01d8907ead1319d65f1591a9a24537c3a474a
tjones61 <tjones61@10.18.12.31>
2017-08-29 15:41:07 -0500

    added second statement to main

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -11,6 +11,7 @@ public class FirstGit {
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("second comment");
     }
     
 }
