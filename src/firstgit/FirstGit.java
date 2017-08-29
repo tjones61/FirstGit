aa5ec1f830dacf6b08fe44c204ef3e104f8ab595
tjones61 <tjones61@10.18.12.31>
2017-08-29 15:45:33 -0500

    after error

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package firstgit;
 
 /**
@@ -10,8 +15,10 @@ public class FirstGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
         System.out.println("Hello world");
         System.out.println("second comment");
+
     }
     
 }
